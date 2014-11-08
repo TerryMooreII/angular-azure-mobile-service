79849223961ce2b6e7c4813960cac0bf62cf8e07
TerryMooreII <terry.moore.ii@gmail.com>
2014-11-08 13:08:18 -0500

    Removed Dash

--- angular-azure-mobile-service.js
+++ angular-azure-mobile-service.js
@@ -4,7 +4,7 @@ angular.module('azure-mobile-service.module', [])
   .service('Azureservice', function Azureservice($q) {
 
     var API_URL = 'https://<AZURE_APP_NAME>.azure-mobile.net/';
--   var API_KEY = '<AZURE_APP_API_KEY>';
+    var API_KEY = '<AZURE_APP_API_KEY>';
     var VAILD_OAUTH_PROVIDERS = ['google', 'twitter', 'facebook', 'microsoftaccount', 'aad'];
 
     var MobileServiceClient = WindowsAzure.MobileServiceClient;
